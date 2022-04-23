c695e44bbf475ace0b82aefad0692fef79e8923d
komron2000 <rayimboevk@gmail.com>
2022-04-23 15:42:01 +0300

    Initial first commit from android

---
+++ app/src/main/java/com/example/first_app/DisplayMessageActivity.java
@@ -0,0 +1,36 @@
+package com.example.first_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.Placeholder;
+
+import android.os.Bundle;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment()).commit();
+        }
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public static class PlaceholderFragment extends Fragment {
+        public PlaceholderFragment() { }
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_display_message,
+                    container, false);
+            return rootView;
+        }
+    }
+}
